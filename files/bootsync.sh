84da9959de239c4c8956261f17b8c04d00887a19
Anders F Björklund <anders.f.bjorklund@gmail.com>
2018-11-14 22:56:18 +0100

    Add updated bootsync.sh script
    Now with autoformat and openssh

---
+++ files/bootsync.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# put other system startup commands here, the boot process will wait until they complete.
+# Use bootlocal.sh for system startup commands that can run in the background 
+# and therefore not slow down the boot process.
+/usr/bin/sethostname box
+
+/etc/init.d/autoformat start
+
+su "tc" -c "tce-load -i podman.tcz varlink.tcz resolver.tcz buildah.tcz"
+/sbin/ldconfig 2>/dev/null
+
+/usr/local/etc/init.d/varlink start
+/usr/local/etc/init.d/podman start
+
+su "tc" -c "tce-load -i openssh.tcz"
+/sbin/ldconfig 2>/dev/null
+
+for keyType in rsa dsa ecdsa ed25519; do # pre-generate a few SSH host keys to decrease the verbosity of /usr/local/etc/init.d/openssh
+	keyFile="/usr/local/etc/ssh/ssh_host_${keyType}_key"
+	[ ! -f "$keyFile" ] || continue
+	echo "Generating $keyFile"
+	ssh-keygen -q -t "$keyType" -N '' -f "$keyFile"
+done
+cp /usr/local/etc/ssh/sshd_config.orig /usr/local/etc/ssh/sshd_config
+/usr/local/etc/init.d/openssh start
+
+if [ -e /var/lib/boot2podman/bootlocal.sh ]; then
+	sh /var/lib/boot2podman/bootlocal.sh &
+fi
+
+/opt/bootlocal.sh &
