49758753b31f217abb2a8b4b4b886dccdcc8c264
Anders F Björklund <anders.f.bjorklund@gmail.com>
2019-01-21 21:23:39 +0100

    Add container compile and package build script

---
+++ packages/build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+go_version=1.10.4
+
+sudo podman container exists boot2podman-build \
+	|| sudo podman run -d --name=boot2podman-build boot2podman-docker-tinycore.bintray.io/tinycore-go:$go_version sleep 3600
+$(sudo podman inspect --format '{{.State.Running}}' boot2podman-build) \
+	|| sudo podman start boot2podman-build
+
+sudo podman exec boot2podman-build sh -x < compile_all
+sudo podman exec boot2podman-build sh -x < package_all
+
+mnt=$(sudo podman mount boot2podman-build)
+sudo sh -c "cp -p ${mnt}/home/tc/*.tcz* ."
+sudo podman umount boot2podman-build
