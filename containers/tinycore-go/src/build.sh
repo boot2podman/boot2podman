ac7a2220cea677dd86d695c0bffd3476d08b011e
Anders F Björklund <anders.f.bjorklund@gmail.com>
2020-08-19 23:55:12 +0200

    Upgrade the go container image to 1.13.8

--- containers/tinycore-go/src/build.sh
+++ containers/tinycore-go/src/build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 sudo podman container exists boot2podman-go \
-	|| sudo podman run -d --name=boot2podman-go boot2podman-docker-tinycore.bintray.io/tinycore-compiletc:10.0-x86_64 sleep 3600
+	|| sudo podman run -d --name=boot2podman-go boot2podman-docker-tinycore.bintray.io/tinycore-compiletc:11.0-x86_64 sleep 3600
 $(sudo podman inspect --format '{{.State.Running}}' boot2podman-go) \
 	|| sudo podman start boot2podman-go
 
-sudo podman exec boot2podman-go sh -x < compile_go
-sudo podman exec boot2podman-go sh -x < package_go
+sudo podman exec -i boot2podman-go sh -x < compile_go
+sudo podman exec -i boot2podman-go sh -x < package_go
 
 mnt=$(sudo podman mount boot2podman-go)
 sudo sh -c "cp -p ${mnt}/home/tc/*.tcz* ."
